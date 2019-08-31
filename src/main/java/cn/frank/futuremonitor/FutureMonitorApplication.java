e535f9250c27a5adad601569bbefa28d461fec89
Frank <frankzhou2018@gmail.com>
2019-08-31 16:21:29 +0800

    init commit

---
+++ src/main/java/cn/frank/futuremonitor/FutureMonitorApplication.java
@@ -0,0 +1,13 @@
+package cn.frank.futuremonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/frank/futuremonitor/FutureMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.frank.futuremonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FutureMonitorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
